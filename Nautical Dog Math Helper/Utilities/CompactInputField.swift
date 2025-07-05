0c4ad4ca243537872463ff907ce8ff68b8a4ee82
Jill Russell <jillfrussell@gmail.com>
2025-07-04 22:47:52 -0800

    New icon, new watch schedule view, new card layout in each view

---
+++ Nautical Dog Math Helper/Utilities/CompactInputField.swift
@@ -0,0 +1,8 @@
+//
+//  CompactInputField.swift
+//  Nautical Dog Math Helper
+//
+//  Created by Jill Russell on 7/4/25.
+//
+
+import Foundation
---
+++ Nautical Dog Math Helper/Utilities/ModernButtonStyle.swift
@@ -0,0 +1,8 @@
+//
+//  ModernButtonStyle.swift
+//  Nautical Dog Math Helper
+//
+//  Created by Jill Russell on 7/4/25.
+//
+
+import Foundation
